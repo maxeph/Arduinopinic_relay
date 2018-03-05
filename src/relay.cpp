8aec77ce7c3c91ef05e6e49a97fb5c9fc0fbdfbe
Stephane M <s.manguette@gmail.com>
2018-03-06 00:29:17 +0100

    Receipt working. Next : CRC16

--- src/relay.cpp
+++ src/relay.cpp
@@ -4,9 +4,11 @@
 
 #define DEBUG 1 // if 1, debug with Serial
 #define RX_433 2 // Pin connecter to Transmitter
+#define MSGLEN 4
+#define PCKTLEN MSGLEN+1
 
 
-byte buffer[4] = {6,7,8,9}; // init unsigned bytes to be sent over
+byte buffer[PCKTLEN+1] = {}; // init unsigned bytes to be sent over
 
 union intarray { // shared memory for int and byte array to get its bytes
   int ints;
@@ -20,10 +22,19 @@ result.part[1] = arg[1];
 return result.ints;
 }
 
+void splitpacket(byte msg[PCKTLEN], byte part1[2], byte part2[2]) { // build array to be sent
+  part1[0] = msg[1];
+  part1[1] = msg[2];
+  part2[0] = msg[3];
+  part2[1] = msg[4];
+  }
+
+intarray itempext, itempeau;
+
 void setup() {
 
   man.setupReceive(RX_433, MAN_600);
-  man.beginReceiveArray(4, buffer);
+  man.beginReceiveArray(PCKTLEN, buffer);
 
   if (DEBUG) {  // Sending over Serial to make sure it works
     Serial.begin(9600);
@@ -39,9 +50,17 @@ void loop() {
 
     //do something with the data in 'buffer' here before you start receiving to the same buffer again
     receivedSize = buffer[0];
-    for(uint8_t i=1; i<receivedSize; i++)
-        Serial.println(buffer[i]);
+    Serial.println("Raw data : ");
+    for(uint8_t i=1; i<receivedSize; i++) {
+      Serial.println(buffer[i]);
+      }
+      splitpacket(buffer, itempext.part, itempeau.part);
+      Serial.print("Température extérieure : ");
+      Serial.println(float(itempext.ints)/100);
+      Serial.print("Température eau : ");
+      Serial.println(float(itempeau.ints)/100);
+      Serial.println("#######################################");
 
-    man.beginReceiveArray(4, buffer);
+    man.beginReceiveArray(PCKTLEN, buffer);
   }
 }
