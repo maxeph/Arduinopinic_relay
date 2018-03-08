a073c5e79c41a3853c69777b2eaaebe669ff1894
Stephane M <s.manguette@gmail.com>
2018-03-08 01:29:32 +0100

    FastCRC ok

--- src/relay.cpp
+++ src/relay.cpp
@@ -1,20 +1,22 @@
 #include <Arduino.h>
 #include <Manchester.h>  //Initialising 433 wireless library
+#include <FastCRC.h>  //CRCcheck library
 
 
 #define DEBUG 1 // if 1, debug with Serial
 #define RX_433 2 // Pin connecter to Transmitter
-#define MSGLEN 4
-#define PCKTLEN MSGLEN+1
+#define MSGLEN 4 // Msg len is 4 = 2 signed int (2 bytes each)
+#define PCKTLEN MSGLEN+3 // +1 for the lenght of the msgpacket +2 for CRC 16
 
 
 byte buffer[PCKTLEN] = {}; // init unsigned bytes to be sent over
 
+FastCRC16 CRC16;
 union intarray { // shared memory for int and byte array to get its bytes
   int ints;
   byte part[2];
 };
-
+intarray itempext, itempeau, crcc, crcc_r;
 int bytes2int(byte arg[2]) { // Convert a 2byte array into int
 intarray result;
 result.part[0] = arg[0];
@@ -28,8 +30,6 @@ void splitpacket(byte msg[PCKTLEN], byte part1[2], byte part2[2]) { // build arr
   part2[0] = msg[3];
   part2[1] = msg[4];
   }
-
-intarray itempext, itempeau;
 
 void setup() {
 
@@ -51,8 +51,8 @@ void loop() {
     //do something with the data in 'buffer' here before you start receiving to the same buffer again
     receivedSize = buffer[0];
     Serial.println("Raw data : ");
-    for(uint8_t i=1; i<receivedSize; i++) {
-      Serial.println(buffer[i]);
+    for(uint8_t i=0; i<receivedSize; i++) {
+      Serial.println(buffer[i],HEX);
       }
       splitpacket(buffer, itempext.part, itempeau.part);
       Serial.print("Température extérieure : ");
@@ -60,6 +60,20 @@ void loop() {
       Serial.print("Température eau : ");
       Serial.println(float(itempeau.ints)/100);
       Serial.println("#######################################");
+      crcc_r.part[0]= buffer[5];
+      crcc_r.part[1]= buffer[6];
+      buffer[5] = 0;
+      buffer[6] = 0;
+
+      crcc.ints = CRC16.ccitt(buffer, sizeof(buffer));
+      Serial.print(crcc.part[0],HEX);
+      Serial.println(crcc.part[1],HEX);
+      Serial.print(crcc_r.part[0],HEX);
+      Serial.println(crcc_r.part[1],HEX);
+            if (crcc.ints == crcc_r.ints) {
+        Serial.println("CRC Check ok");
+      }
+
 
     man.beginReceiveArray(PCKTLEN, buffer);
   }
