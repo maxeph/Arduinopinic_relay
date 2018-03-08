fa2108ded98162ff3d5b570b8aaa9401aa47e0eb
Stephane M <s.manguette@gmail.com>
2018-03-08 01:32:56 +0100

    comment and autoindent

--- src/relay.cpp
+++ src/relay.cpp
@@ -2,26 +2,33 @@
 #include <Manchester.h>  //Initialising 433 wireless library
 #include <FastCRC.h>  //CRCcheck library
 
+// Declaring definitions
 
 #define DEBUG 1 // if 1, debug with Serial
 #define RX_433 2 // Pin connecter to Transmitter
 #define MSGLEN 4 // Msg len is 4 = 2 signed int (2 bytes each)
 #define PCKTLEN MSGLEN+3 // +1 for the lenght of the msgpacket +2 for CRC 16
 
+// Declaring structs
 
-byte buffer[PCKTLEN] = {}; // init unsigned bytes to be sent over
-
-FastCRC16 CRC16;
 union intarray { // shared memory for int and byte array to get its bytes
   int ints;
   byte part[2];
 };
+
+// Declaring variables
+
+byte buffer[PCKTLEN] = {}; // init unsigned bytes to be sent over
+FastCRC16 CRC16;
 intarray itempext, itempeau, crcc, crcc_r;
+
+// Declaring functions
+
 int bytes2int(byte arg[2]) { // Convert a 2byte array into int
-intarray result;
-result.part[0] = arg[0];
-result.part[1] = arg[1];
-return result.ints;
+  intarray result;
+  result.part[0] = arg[0];
+  result.part[1] = arg[1];
+  return result.ints;
 }
 
 void splitpacket(byte msg[PCKTLEN], byte part1[2], byte part2[2]) { // build array to be sent
@@ -29,7 +36,7 @@ void splitpacket(byte msg[PCKTLEN], byte part1[2], byte part2[2]) { // build arr
   part1[1] = msg[2];
   part2[0] = msg[3];
   part2[1] = msg[4];
-  }
+}
 
 void setup() {
 
@@ -40,8 +47,8 @@ void setup() {
     Serial.begin(9600);
     Serial.println("Initialising debug mode...");
     Serial.println("Received packets : ");
-    }
   }
+}
 
 void loop() {
   if (man.receiveComplete())
@@ -53,26 +60,26 @@ void loop() {
     Serial.println("Raw data : ");
     for(uint8_t i=0; i<receivedSize; i++) {
       Serial.println(buffer[i],HEX);
-      }
-      splitpacket(buffer, itempext.part, itempeau.part);
-      Serial.print("Température extérieure : ");
-      Serial.println(float(itempext.ints)/100);
-      Serial.print("Température eau : ");
-      Serial.println(float(itempeau.ints)/100);
-      Serial.println("#######################################");
-      crcc_r.part[0]= buffer[5];
-      crcc_r.part[1]= buffer[6];
-      buffer[5] = 0;
-      buffer[6] = 0;
+    }
+    splitpacket(buffer, itempext.part, itempeau.part);
+    Serial.print("Température extérieure : ");
+    Serial.println(float(itempext.ints)/100);
+    Serial.print("Température eau : ");
+    Serial.println(float(itempeau.ints)/100);
+    Serial.println("#######################################");
+    crcc_r.part[0]= buffer[5];
+    crcc_r.part[1]= buffer[6];
+    buffer[5] = 0;
+    buffer[6] = 0;
 
-      crcc.ints = CRC16.ccitt(buffer, sizeof(buffer));
-      Serial.print(crcc.part[0],HEX);
-      Serial.println(crcc.part[1],HEX);
-      Serial.print(crcc_r.part[0],HEX);
-      Serial.println(crcc_r.part[1],HEX);
-            if (crcc.ints == crcc_r.ints) {
-        Serial.println("CRC Check ok");
-      }
+    crcc.ints = CRC16.ccitt(buffer, sizeof(buffer));
+    Serial.print(crcc.part[0],HEX);
+    Serial.println(crcc.part[1],HEX);
+    Serial.print(crcc_r.part[0],HEX);
+    Serial.println(crcc_r.part[1],HEX);
+    if (crcc.ints == crcc_r.ints) {
+      Serial.println("CRC Check ok");
+    }
 
 
     man.beginReceiveArray(PCKTLEN, buffer);
